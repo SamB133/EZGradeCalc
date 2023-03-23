dab4f79769bdcca179f684558c9cf72c89fc8315
SamB133 <64711289+SamB133@users.noreply.github.com>
2023-03-22 23:03:08 -0500

    Initial Commit

---
+++ GradingCalculator/GradingCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradingCalculatorApp.swift
+//  GradingCalculator
+//
+//  Created by Samuel A. Benicewicz on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GradingCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
